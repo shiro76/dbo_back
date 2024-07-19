1d9cf6d8fc3049daff7016eee8506a641ef7af16
shiro76 <unconfigured@null.spigotmc.org>
2024-07-19 23:57:41 +0200

    add regex

--- middleware/validator.js
+++ middleware/validator.js
@@ -2,13 +2,15 @@ const { body, validationResult } = require('express-validator');
 
 const validateSignup = [
     body('username')
-        .isLength({ min: 3 }).withMessage('Username must be at least 3 characters long')
+        .isLength({ min: 3 }).withMessage("Nom d'Utilisateur doit contenir au moin 3 caractère")
         .trim().escape(),
     body('email')
-        .isEmail().withMessage('Please enter a valid email address')
+        .isEmail().withMessage('entrer une address email valide')
         .normalizeEmail(),
     body('password')
-        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+        .isLength({ min: 8 }).withMessage('le mot de passe doit contenir 8 caractère minimum')
+        .matches(/[A-Z]/).withMessage('le mot de passe doit contenir au moin une majuscule')
+        .matches(/[!@#$%^&*(),.?":{}|<>]/).withMessage('le mot de passe doit contenir un caractère special')
         .escape(),
     body('password2')
         .custom((value, { req }) => {
@@ -49,8 +51,6 @@ module.exports = {
 
 
 
-
-
 // const { body, validationResult } = require('express-validator');
 
 // const validateSignup = [
@@ -78,3 +78,24 @@ module.exports = {
 //         next();
 //     }
 // ];
+
+// const validateLogin = [
+//     body('username')
+//         .notEmpty().withMessage('Username is required')
+//         .trim().escape(),
+//     body('password')
+//         .notEmpty().withMessage('Password is required')
+//         .escape(),
+//     (req, res, next) => {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() });
+//         }
+//         next();
+//     }
+// ];
+
+// module.exports = {
+//     validateSignup,
+//     validateLogin
+// };
