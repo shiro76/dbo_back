701bb45ee8fce721e4b1afceac5c23709e6330cf
shiro76 <unconfigured@null.spigotmc.org>
2024-07-22 22:43:09 +0200

    helmet-add

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const path = require('path'); // Ajoutez cette ligne
 const userRoutes = require('./routes/user.js')
 const homeRoutes = require('./routes/home.js');
 const adminRoutes = require('./routes/admin.js');
 
 const app = express();
+
+// Utiliser helmet pour définir des en-têtes de sécurité
+app.use(helmet());
+
+// Ou configurer individuellement les modules de helmet
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts({
+    maxAge: 31536000, // 1 an
+    includeSubDomains: true,
+    preload: true
+}));
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.dnsPrefetchControl({ allow: false }));
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'trusted-cdn.com'"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: []
+    }
+}));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
